90f08ae0468df06789816c926b6981738aea9c8a
Lysida Santiago <lysida.santiago@gmail.com>
2019-11-24 14:27:10 -0500

    modified bmi calculator

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -4,13 +4,19 @@
 weight = int(input("What is your weight (in lbs)? "))
 height = int(input("What is your height (in inches)? "))
 
-bmi_formula = round(float(weight / (height * height) * 703))
-bmi = int(bmi_formula)
+bmi = round(float(weight / (height * height) * 703), 2)
 
 print("Your weight is " + str(weight) + " and your height is " + str(height))  
 print("Your BMI is " + str(bmi))
 
-if bmi <= 18.5: print("You are underweight. Eat more!")
-elif bmi >= 18.5 and bmi <= 24.9: print("You have a normal weight. Good job!")
-elif bmi >= 25 and bmi <= 29.9: print("You are overweight. Go workout fatty!")
-elif bmi >= 30: print("You are obese. Eat healthier!")
+def calculate_bmi():
+	if bmi <= 18.5: 
+		print("You are underweight. Eat more!")
+	elif bmi >= 18.5 and bmi <= 24.9: 
+		print("You have a normal weight. Good job!")
+	elif bmi >= 25 and bmi <= 29.9: 
+		print("You are overweight. Go workout fatty!")
+	elif bmi >= 30: 
+		print("You are obese. Eat healthier!")
+
+calculate_bmi()
