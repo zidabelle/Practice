7cd5b08c9a940aff3645f265b62b850fd2cc101f
James Santiago <jameslyle.santiago@gmail.com>
2019-12-01 10:41:00 -0500

    divsors practice problem

---
+++ divisors.py
@@ -0,0 +1,4 @@
+# Creage a program that asks the user to input a number and determine the divsisors for the given mumber.
+# Ex: if a user inputs 12, the program should display 2, 3, 4, 6. If a user inputs 15, the output should be 3, 5.
+
+
